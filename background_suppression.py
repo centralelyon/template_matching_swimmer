feba51ab066bc139a01ca9908e24aebfdaee9b7b
tcourat <82943048+tcourat@users.noreply.github.com>
2021-05-09 17:55:15 +0200

    Add files via upload

--- background_suppression.py
+++ background_suppression.py
@@ -1,5 +1,23 @@
 import cv2 
-from reflets import corriger_reflet
+
+
+
+def corriger_reflet(img, threshold, inpaintradius) :
+     
+    # Thresholding
+    mask = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY ), threshold, 255, cv2.THRESH_BINARY)[1]
+    
+    # Closing 
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+    
+    # Inpainting
+    result = cv2.inpaint(img, mask, inpaintradius, cv2.INPAINT_TELEA) 
+
+    # return the corrected image
+    return result
 
 
 def background_suppression(videopath, dist) :
